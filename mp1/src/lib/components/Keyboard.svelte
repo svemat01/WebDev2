b119f6c5629ccdd236e7cd57977b1279b9aa138a
Svemat01 <jakob.helgesson@gmail.com>
2022-10-04 08:48:04 +0200

    Remove commented out code

--- mp1/src/lib/components/Keyboard.svelte
+++ mp1/src/lib/components/Keyboard.svelte
@@ -6,27 +6,6 @@
 </script>
 
 <section>
-    <!-- <button id="b7">7</button>
-    <button id="b8">8</button>
-    <button id="b9">9</button>
-    <button id="add">+</button>
-
-    <button id="b4">4</button>
-    <button id="b5">5</button>
-    <button id="b6">6</button>
-    <button id="sub">-</button>
-
-    <button id="b1">1</button>
-    <button id="b2">2</button>
-    <button id="b3">3</button>
-    <button id="mul">x</button>
-
-    <button id="comma">,</button>
-    <button id="b0">0</button>
-    <button id="enter">=</button>
-    <button id="div">/</button>
-
-    <button id="clear" style="grid-column: span 4">CLEAR</button> -->
 
     <Button onClick={() => $valueStore += '7'}>7</Button>
     <Button onClick={() => $valueStore += '8'}>8</Button>
@@ -49,7 +28,6 @@
     <Button onClick={() => setOperator('/')} style={'color: #5389fd'}>/</Button>
 
     <Button onClick={() => $valueStore == '' ? memClear() : clearLCD()} style="grid-column: span 4; color: #5389fd">{$valueStore == '' ? 'RESET' : 'CLEAR'}</Button>
-    <!-- <button id="clear" CLEAR</button> -->
 </section>
 
 <style lang="scss">
